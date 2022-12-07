59332e6995c2f7e2f9906c90234e996aea934090
andy817501 <3065774543@qq.com>
2022-12-07 11:50:01 +0800

    senve

--- sidebars.js
+++ sidebars.js
@@ -24,9 +24,72 @@ const sidebars = {
                 type: 'generated-index',
             },
             items: [
-                'MCU_TEACH',
+                'MCU_TEACH_TIMERGET',
+                'MCU_IRN_YINGJIAN',
+                'STM-STUDIO',
             ],
         },
+        {
+            type: 'category',
+            label: '通讯协议',
+            link: {
+                type: 'generated-index',
+            },
+            items: [
+                'USART',
+                'IIC',
+                'SPI',
+                'CAN',
+                'MODBUS',
+                'TCP/IP',
+            ],
+        },
+        {
+            type: 'category',
+            label: '嵌入式-RTOS',
+            link: {
+                type: 'generated-index',
+            },
+            items: [
+                'RTT_ROSMCU',
+                'UART_USE',
+                'ENV_USE',
+            ],
+        },
+        {
+            type: 'category',
+            label: '嵌入式-Linux',
+            link: {
+                type: 'generated-index',
+            },
+            items: [
+                'LINUX_UBUNTU',
+                'LINUX_KNOLEW',
+            ],
+        },
+        {
+            type: 'category',
+            label: '机器人_ROS',
+            link: {
+                type: 'generated-index',
+            },
+            items: [
+                'ros_moveit',
+                'ros_Slam',
+                'ros_serial',
+            ],
+        },
+        {
+            type: 'category',
+            label: '机器人_算法',
+            link: {
+                type: 'generated-index',
+            },
+            items: [
+                'pid',
+            ],
+        },
+        
     ],
 };
 
